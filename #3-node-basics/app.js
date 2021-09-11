887538488fed2c169c64a4ec8100d8690dd7833e
Samarjeetsingh Gaidhu <61595281+samarjeetr2001@users.noreply.github.com>
2021-09-11 12:39:43 +0530

    node moble system

--- #3-node-basics/app.js
+++ #3-node-basics/app.js
@@ -1,44 +1,10 @@
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes");
 
-const server = http.createServer((req, res)=>{
-    // console.log("request");
-    // console.log(req.url, req.method, req.headers);
-    // console.log(req);
-    const url = req.url;
-    const method = req.method;
-    if(url ==="/"){
-        res.write("<html>");
-        res.write("<head><title>Base page</title></head>");
-        res.write("<body> <form action='/message' method = 'POST'><input type='text' name='input1'><button type='submit'>Send</button></form></body>");
-        res.write("</html>");
-       return  res.end();
-    }
-    if(url==="/message" && method==='POST'){
-        const body = [];
-        // listen lchucks coming in request
-        req.on("data", (chunk)=>{
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on("end", ()=>{
-            const parseBody = Buffer.concat(body).toString();
-            console.log(parseBody);
-            const message = parseBody.split("=")[1];
-            fs.writeFileSync("message.txt",message);
-        });
+// const server = http.createServer((req, res)=>{
+// });
 
-        res.statusCode = 302;
-        res.setHeader('Location', '/');// redirecting request
-        return res.end();
-    //    res.writeHead(302, {"Location": "/"}); // this equivalant to above 2 lines
-    }
-    res.setHeader( "Content-type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>First page</title></head>");
-    res.write("<body><h1> hello from node application </body>");
-    res.write("</html>");
-    return res.end();
-});
+console.log(routes.someText);
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
