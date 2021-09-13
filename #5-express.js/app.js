c7db816f0fe5c0bd3d68d2701d6b73444b9ac52c
Samarjeetsingh Gaidhu <61595281+samarjeetr2001@users.noreply.github.com>
2021-09-13 16:06:34 +0530

    limiting middleware execution to post method

--- #5-express.js/app.js
+++ #5-express.js/app.js
@@ -13,7 +13,20 @@ app.use('/add-product',(req, res, next)=>{
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">submit</button></form>');
 });
 
-app.use('/product',(req, res, next)=>{
+// app.use('/product',(req, res, next)=>{
+//     console.log(req.body);
+//     res.redirect('/');
+// });
+
+// app.get('/product',(req, res, next)=>{
+//     console.log(req.body);
+//     console.log("product get function");
+// });
+// similarly we have for delte, update, add
+
+
+app.post('/product',(req, res, next)=>{
+    console.log("product post function");
     console.log(req.body);
     res.redirect('/');
 });
