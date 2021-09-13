333607804acc5d99d61c0f92c654db3a6f54d2bd
Samarjeetsingh Gaidhu <61595281+samarjeetr2001@users.noreply.github.com>
2021-09-13 15:35:22 +0530

    routing

--- #5-express.js/app.js
+++ #5-express.js/app.js
@@ -1,17 +1,20 @@
-const http = require("http");
-//
 const express = require("express");
 //
 
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log("In the middleware");
-    next();
+// app.use('/', (req, res, next)=>{ 
+//     console.log("This always run");
+//     next();
+// });
+
+app.use('/add-product',(req, res, next)=>{
+    console.log("In the middleware of add product ");
+    res.send("<h1>Hello from Add Product page</h1>");
 });
 
-app.use((req, res, next)=>{
-    console.log("In another the middleware");
+app.use('/',(req, res, next)=>{
+    console.log("In the middleware default last");
     res.send("<h1>Hello from Express app</h1>");
 });
 
