a0a46eac8b02c36486e628bc544dbd3d874f1829
Samarjeetsingh Gaidhu <61595281+samarjeetr2001@users.noreply.github.com>
2021-09-17 12:37:15 +0530

    express-handlebars

--- #6-dynamic-content-&-templates/routes/admin.js
+++ #6-dynamic-content-&-templates/routes/admin.js
@@ -11,7 +11,7 @@ const products = [];
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product', productCSS: true, formsCSS: true, activeAddProduct: true });
 });
 
 // /admin/add-product => POST
