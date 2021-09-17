a5ef816f4f246f2ac5fdda189c3024d25b39b8b4
Samarjeetsingh Gaidhu <61595281+samarjeetr2001@users.noreply.github.com>
2021-09-17 13:36:26 +0530

    fix error in 404.ejs

--- #6-dynamic-content-&-templates/app.js
+++ #6-dynamic-content-&-templates/app.js
@@ -25,7 +25,7 @@ app.use('/admin', adminData.router);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page not Found' });
+    res.status(404).render('404', { pageTitle: 'Page not Found', path: req.url });
 });
 
 app.listen(3000);
