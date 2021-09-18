197bce2b0aaafdcec92e0d52cf0751e85f8e2473
Samarjeetsingh Gaidhu <61595281+samarjeetr2001@users.noreply.github.com>
2021-09-18 16:15:59 +0530

    storaing data to file

--- #7-model-view-controller/models/product.js
+++ #7-model-view-controller/models/product.js
@@ -1,4 +1,20 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+const dirPath = require('../util/path');
+
+const p = path.join(dirPath, 'data', 'products.json');
+
+const getAllProductsFromFile = cb => {
+    fs.readFile(p, (err, data) => {
+        if (err) {
+            cb([]);
+        } else {
+            let products = [];
+            products = JSON.parse(data);
+            cb(products);
+        }
+    });
+};
 
 module.exports = class Product {
     constructor(t) {
@@ -6,10 +22,15 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        getAllProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err => {
+                console.log(err);
+            });
+        });
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        getAllProductsFromFile(cb);
     }
 };
