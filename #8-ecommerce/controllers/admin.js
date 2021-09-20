baa565dd20917d2e82a334bb3375c55aa27f382e
Samarjeetsingh Gaidhu <61595281+samarjeetr2001@users.noreply.github.com>
2021-09-20 17:12:35 +0530

    edit product

--- #8-ecommerce/controllers/admin.js
+++ #8-ecommerce/controllers/admin.js
@@ -4,9 +4,6 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
   });
 };
 
@@ -15,10 +12,39 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const productId = req.params.productId;
+  Product.findById(productId, product => {
+    console.log(product);
+    if (!product) return res.redirect('/');
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      product: product
+    });
+  });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const productId = req.body.productId;
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(productId, title, imageUrl, description, price);
+  product.save();
+  res.redirect('/admin/products');
+};
+
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
