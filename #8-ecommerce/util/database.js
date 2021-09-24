3b1a2af1a90746c0f5b0d4237e7f90bb65a94eb7
Samarjeetsingh Gaidhu <61595281+samarjeetr2001@users.noreply.github.com>
2021-09-24 14:30:13 +0530

    product model in sequelize

--- #8-ecommerce/util/database.js
+++ #8-ecommerce/util/database.js
@@ -1,10 +1,17 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Samarjeet1!'
-});
+const sequelize = new Sequelize('node-complete', 'root', 'Samarjeet1!', { dialect: 'mysql', host: 'localhost' });
 
-module.exports = pool.promise();
+module.exports = sequelize;
+
+
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password: 'Samarjeet1!'
+// });
+
+// module.exports = pool.promise();
