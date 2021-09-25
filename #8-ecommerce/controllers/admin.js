f81bb5a4a5b33e231bcd8671cbdeea7da9656faf
Samarjeetsingh Gaidhu <61595281+samarjeetr2001@users.noreply.github.com>
2021-09-25 15:15:19 +0530

    cart model created

--- #8-ecommerce/controllers/admin.js
+++ #8-ecommerce/controllers/admin.js
@@ -13,12 +13,20 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  //  special function createProduct created by sequelize due to association
+  req.user.createProduct({
     title: title,
     price: price,
     imageUrl: imageUrl,
     description: description,
   })
+    // Product.create({
+    //   title: title,
+    //   price: price,
+    //   imageUrl: imageUrl,
+    //   description: description,
+    //   userId: req.user.id,
+    // })
     .then((result) => {
       res.redirect('/admin/products');
     })
