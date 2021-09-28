fd0b7e34fde67b7b949bc6cb8d3d00870d21bbb1
Samarjeetsingh Gaidhu <61595281+samarjeetr2001@users.noreply.github.com>
2021-09-28 13:32:29 +0530

    nsql setup

---
+++ #9-ecommerce-nosql/util/database.js
@@ -0,0 +1,29 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const _db;
+
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://Samarjeet:Samarjeet1!@cluster0.rcigb.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'DB not found';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
