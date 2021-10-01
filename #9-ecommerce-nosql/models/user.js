322987d63537292b37db91536f5cf7f78fac7bb2
Samarjeetsingh Gaidhu <61595281+samarjeetr2001@users.noreply.github.com>
2021-10-01 15:41:29 +0530

    cart

--- #9-ecommerce-nosql/models/user.js
+++ #9-ecommerce-nosql/models/user.js
@@ -3,9 +3,11 @@ const mongoDb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart; // {items : []}
+    this._id = id;
   }
 
   save() {
@@ -28,6 +30,48 @@ class User {
       .catch(err => {
         console.log(err);
       });
+  }
+
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map(i => {
+      return i.productId;
+    });
+    return db.collection('products').find({ _id: { $in: productIds } }).toArray()
+      .then(products => {
+        return products.map(product => {
+          return {
+            ...product,
+            quantity: this.cart.items.find(i => {
+              return i.productId.toString() === product._id.toString();
+            }).quantity
+          };
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  addToCart(product) {
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId.toString() === product._id.toString();
+    });
+
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({ productId: product._id, quantity: newQuantity });
+    }
+
+    const updatedCart = { items: updatedCartItems };
+
+    const db = getDb();
+    return db.collection('users').updateOne({ _id: new mongoDb.ObjectId(this._id) }, { $set: { cart: updatedCart } });
   }
 }
 
