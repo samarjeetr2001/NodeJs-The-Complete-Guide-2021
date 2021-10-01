74c29e823b72d3cfc50ac7ffd5067f121c6d6ee4
Samarjeetsingh Gaidhu <61595281+samarjeetr2001@users.noreply.github.com>
2021-10-01 13:28:40 +0530

    add, update, delete and get products from mongodb

--- #9-ecommerce-nosql/controllers/admin.js
+++ #9-ecommerce-nosql/controllers/admin.js
@@ -31,11 +31,9 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  req.user
-    .getProducts({ where: { id: prodId } })
-    // Product.findById(prodId)
-    .then(products => {
-      const product = products[0];
+
+  Product.findById(prodId)
+    .then(product => {
       if (!product) {
         return res.redirect('/');
       }
@@ -51,18 +49,12 @@ exports.getEditProduct = (req, res, next) => {
 
 exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  const updatedTitle = req.body.title;
-  const updatedPrice = req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
-  const updatedDesc = req.body.description;
-  Product.findById(prodId)
-    .then(product => {
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
-      return product.save();
-    })
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(title, price, description, imageUrl, prodId);
+  product.save()
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
@@ -84,10 +76,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId)
-    .then(product => {
-      return product.destroy();
-    })
+  Product.deleteById(prodId)
     .then(result => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
--- #9-ecommerce-nosql/models/product.js
+++ #9-ecommerce-nosql/models/product.js
@@ -1,28 +1,60 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
 
   save() {
     const db = getDb();
-    return db.collection('products').insertOne(this)
-      .then(result => {
-        console.log(result);
-      })
-      .catch(err => { console.log(err) });
+    if (this._id) {
+      return db.collection('products').updateOne({ _id: this._id }, { $set: this })
+        .then(result => {
+          console.log(result);
+        })
+        .catch(err => { console.log(err) });
+    } else {
+      return db.collection('products').insertOne(this)
+        .then(result => {
+          console.log(result);
+        })
+        .catch(err => { console.log(err) });
+    }
+
   }
 
   static fetchAll() {
     const db = getDb();
     return db.collection('products').find().toArray()
       .then(products => {
-        console.log(products);
         return products;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static findById(productId) {
+    const db = getDb();
+    return db.collection('products').find({ _id: new mongodb.ObjectId(productId) }).next()
+      .then(product => {
+        return product;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static deleteById(productId) {
+    const db = getDb();
+    return db.collection('products').deleteOne({ _id: new mongodb.ObjectId(productId) })
+      .then(result => {
+        console.log('Deleted!');
       })
       .catch(err => {
         console.log(err);
